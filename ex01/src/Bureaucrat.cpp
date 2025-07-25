9467edca43262163324493f7cb1740ce00bfc53d
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2025-07-25 12:03:18 +0200

    init ex01

---
+++ ex01/src/Bureaucrat.cpp
@@ -0,0 +1,47 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+	: name_(name), grade_(grade) {
+	if (grade < 1) {
+		throw GradeTooHighException();
+	} else if (grade > 150) {
+		throw GradeTooLowException();
+	}
+}
+
+const std::string &Bureaucrat::getName() const
+{
+	return name_;
+}
+
+int Bureaucrat::getGrade() const {
+	return grade_;
+}
+
+void Bureaucrat::incrementGrade() {
+	if (grade_ == 1) {
+		throw GradeTooHighException();
+	}
+	grade_--;
+}
+
+void Bureaucrat::decrementGrade() {
+	if (grade_ == 150) {
+		throw GradeTooLowException();
+	}
+	grade_++;
+}
+
+const char* GradeTooHighException::what() const noexcept {
+	return "Grade is too high!";
+}
+
+const char* GradeTooLowException::what() const noexcept {
+	return "Grade is too low!";
+}
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &b)
+{
+	os << b.getName() << ", bureaucrat grade " << b.getGrade() << ".\n"; 
+	return os;
+}
